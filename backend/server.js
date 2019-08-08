56ec0e77626fb8bf36272d0e4299e2b22c271506
Elliot Boschwitz <elliot.boschwitz@gmail.com>
2019-08-07 19:52:52 -0700

    collects words asynchronously

--- backend/server.js
+++ backend/server.js
@@ -1,5 +1,7 @@
 const express = require('express');
 const path = require('path');
+const wordListPath = require('word-list');
+const fs = require('fs');
 
 const API_PORT = process.env.HTTP_PORT || 4001;
 const app = express();
@@ -8,7 +10,13 @@ app.use(express.static(path.join(__dirname, '../client/build')));
 
 app.get('/', function(req, res) {
 	res.sendFile(path.join(__dirname, '../client/build', 'index.html'));
-  });
+});
+
+// loads roughly 274k words
+app.get('/words', function(req, res) {
+	const words = fs.readFileSync(wordListPath, 'utf-8').split('\n');
+	res.json({ data: words });
+});
 
 // launch our backend into a port
 app.listen(API_PORT, () => console.log(`LISTENING ON PORT ${API_PORT}`));
--- client/src/App.js
+++ client/src/App.js
@@ -1,10 +1,25 @@
-import React from 'react';
-import BloomFilter from './bloomFilter'
+import React, { useState, useEffect } from 'react';
+import BloomFilter from './BloomFilter'
 
 function App() {
+  const [wordsList, setWordsList] = useState(null);
+
+  // fetch list of words
+  useEffect(() => {
+    fetch('/words')
+      .then(res => res.json())
+      .then(
+        (result) => {
+          setWordsList(result.data)
+        }
+      )
+  });
+
   return (
     <div>
-      <BloomFilter />
+      <h1>Hi</h1>
+      {wordsList}
+      {/* <BloomFilter /> */}
     </div>
   );
 }
