727a1a44ae04df5e3a721ac5df40808c0b98bf71
ellbosch <elliot.boschwitz@gmail.com>
2019-08-08 13:31:52 -0700

    added readme
    added bootstrap

---
+++ README.md
@@ -0,0 +1,4 @@
+# Elliot's Bloom Filter
+To build, call <code>npm install</code> from root directory.
+
+To start, call <code>npm start</code> also from root.
--- client/src/index.js
+++ client/src/index.js
@@ -3,6 +3,14 @@ import ReactDOM from 'react-dom';
 import App from './App';
 import * as serviceWorker from './serviceWorker';
 
+// CSS
+import 'bootstrap/dist/css/bootstrap.css';
+
+// JS
+import 'jquery';
+import 'bootstrap/dist/js/bootstrap.js';
+
+
 ReactDOM.render(<App />, document.getElementById('root'));
 
 // If you want your app to work offline and load faster, you can change
