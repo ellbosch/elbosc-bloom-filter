ba6c70384f060787b34e8f32e67c0be16b4e4010
Elliot Boschwitz <elliot.boschwitz@gmail.com>
2019-08-10 21:51:04 -0700

    can update bit vector size from ui

--- backend/server.js
+++ backend/server.js
@@ -1,16 +1,17 @@
-
-const express = require('express');
-const app = express();
-const path = require('path');
 const bloomfilter =  require('./bloomfilter.js');
+const bodyParser = require('body-parser');
+const express = require('express');
+const path = require('path');
 
+const app = express();
 const API_PORT = process.env.HTTP_PORT || 4001;
 
-// instantiate our bloomfilter and create its bitvector
-const bf = new bloomfilter(size=Math.pow(10,8));
-bf.createStore();
+// hold pointer to bloom filter
+var bf = null;
 
 app.use(express.static(path.join(__dirname, '../client/build')));
+app.use(bodyParser.json());
+// app.use(bodyParser.urlencoded({ extended: true }));
 
 app.get('/', function(req, res) {
 	res.sendFile(path.join(__dirname, '../client/build', 'index.html'));
@@ -18,6 +19,19 @@ app.get('/', function(req, res) {
 
 // create router for bloomfilter
 var bfRouter = express.Router();
+
+// creates new bloom filter and sets size of bit vector
+bfRouter.post('/', function(req, res) {
+	const sizePowerOfTen = req.body.size;
+
+	if (sizePowerOfTen != null) {
+		bf = new bloomfilter(size=Math.pow(10, sizePowerOfTen));
+		bf.createStore();
+		res.sendStatus(200);
+	} else {
+		res.sendStatus(405);
+	}
+});
 
 // search word in bloomfilter
 bfRouter.get('/:word', function(req, res) {
--- client/src/App.js
+++ client/src/App.js
@@ -1,4 +1,4 @@
-import React, { useState, useEffect } from 'react';
+import React, { useState, useEffect, useCallback } from 'react';
 
 function App() {
   const [queryString, setQueryString] = useState("");
@@ -6,16 +6,44 @@ function App() {
   return (
     <div className="container text-center">
       <h1>Bloom Filter</h1>
-      <input type="text" className="form-control" placeholder="Look up word in bloom filter" aria-label="Search word" aria-describedby="button-search" value={queryString} onChange={e => setQueryString(e.target.value)} />
+      <input type="text" className="form-control" placeholder="Look up word in bloom filter" aria-label="Search word"
+          aria-describedby="button-search" value={queryString} onChange={e => setQueryString(e.target.value)} />
       <br />
+      <Controls />
       <QueryResult query={queryString} />
     </div>
   );
 }
 
-// function Controls() {
+function Controls() {
+  const [vectorSize, setVectorSize] = useState(8);   // let user control bit vector (set to powers of 10)
+  const createBloomFilter = useCallback(() => {
+    fetch('/bloomfilter', {
+      method: 'post',
+      headers: {'Content-Type':'application/json'},
+      body: JSON.stringify({
+        'size': vectorSize
+      })
+    });
+  }, [vectorSize]);
 
-// }
+  return (
+    <div className="row align-items-center">
+      <div className="col-auto">
+        <div className="input-group mb-2">
+          <div className="input-group-prepend">
+            <span className="input-group-text" id="vector-size-addon">Vector Size in 0's</span>
+          </div>
+          <input type="text" className="form-control" placeholder="Power of 10" aria-label="Vector Size"
+              aria-describedby="vector-size-addon" value={vectorSize} onChange={e => setVectorSize(e.target.value)} />
+          <div className="input-group-append">
+            <button className="btn btn-outline-secondary" type="button" onClick={createBloomFilter}>Create</button>
+          </div>
+        </div>
+      </div>
+    </div>
+  )
+}
 
 function QueryResult(props) {
   const queryString = props.query;
