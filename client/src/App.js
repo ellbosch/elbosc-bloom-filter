194b4ba82d261861c08937b2354aa87d63f37d66
Elliot Boschwitz <elliot.boschwitz@gmail.com>
2019-08-11 12:59:41 -0700

    latest attempt with individual states

--- client/src/App.js
+++ client/src/App.js
@@ -9,16 +9,21 @@ function App() {
       <input type="text" className="form-control" placeholder="Look up word in bloom filter" aria-label="Search word"
           aria-describedby="button-search" value={queryString} onChange={e => setQueryString(e.target.value)} />
       <br />
-      <Controls size={8} setWordLookUpQuery={setQueryString} />
+      <Controls size={8} setWordLookUpQuery={setQueryString} algorithms={{'MD5': true, 'SHA-1': true, 'SHA-256': true}} />
       <QueryResult query={queryString} />
     </div>
   );
 }
 
 function Controls(props) {
-  const [vectorSizeServer, setVectorSizeServer] = useState(props.size);   // current state of vector size from server
-  const [vectorSizeQuery, setVectorSizeQuery] = useState("");             // query string for vector size
+  const [vectorSizeInput, setVectorSizeInput] = useState("");                 // query string for vector size
+  const [vectorSizeServer, setVectorSizeServer] = useState(props.size);       // current state of vector size from server
+  const [md5Checkbox, setMd5Checkbox] = useState(props.algorithms["MD5"]);           // algorithms currently selected on client
+  const [sha1Checkbox, setSha1Checkbox] = useState(props.algorithms["SHA-1"]);         // algorithms currently selected on client
+  const [sha256Checkbox, setSha256Checkbox] = useState(props.algorithms["SHA-256"]);     // algorithms currently selected on client
+  const [algorithmsServer, setAlgorithmsServer] = useState(props.algorithms); // algorithms currently selected on server
 
+  // post request for creating bloom filter
   function createBloomFilter(e) {
     e.preventDefault();
 
@@ -26,7 +31,12 @@ function Controls(props) {
       method: 'post',
       headers: {'Content-Type':'application/json'},
       body: JSON.stringify({
-        'size': vectorSizeQuery
+        'size': vectorSizeInput,
+        'algorithms': {
+          'MD5': md5Checkbox,
+          'SHA-1': sha1Checkbox,
+          'SHA-256': sha256Checkbox
+        }
       })
     })
     .then(res => res.json())
@@ -34,9 +44,31 @@ function Controls(props) {
       (result) => {
         // UI updates with new size of bit vector and removes text from word lookup input
         setVectorSizeServer(result.size);
+        setAlgorithmsServer(result.algorithms);
         props.setWordLookUpQuery("");
       }
     )
+  }
+
+  // function toggleCheckbox(key) {
+  //   var newAlgorithmsInput = algorithmsInput;
+  //   newAlgorithmsInput[key] = !algorithmsInput[key];
+  //   setAlgorithmsInput(newAlgorithmsInput);
+  // }
+
+  // returns string friendly dictionary for true elements
+  function booleanDictToString(dict) {
+    var selected = [];
+
+    for (var key in dict) {
+      if (dict[key]) {
+        selected.push(key);
+      }
+    }
+    if (selected.length == 0) {
+      return "None";
+    }
+    return selected.join(", ");
   }
 
   return (
@@ -45,26 +77,29 @@ function Controls(props) {
         <div className="form-group">
           <label className="mr-2" htmlFor="vector-size-input">Vector Size in 0's</label>
           <input type="text" className="form-control form-control-sm" id="vector-size-input" placeholder="Power of 10" aria-label="Vector Size"
-              value={vectorSizeQuery} onChange={e => setVectorSizeQuery(e.target.value)} />
+              value={vectorSizeInput} onChange={e => setVectorSizeInput(e.target.value)} />
         </div>
         <span><small>Current Bit Vector Size: 10<sup>{vectorSizeServer}</sup></small></span>
       </div>
       <div className="col-auto col-control mb-3">
         <div className="form-group mb-1">
           <div className="form-check form-check-inline">
-            <input className="form-check-input" type="checkbox" id="inlineCheckboxMd5" value="md5" />
+            <input className="form-check-input" type="checkbox" id="inlineCheckboxMd5" checked={md5Checkbox} value="md5"
+                onChange={setMd5Checkbox(!md5Checkbox)} />
             <label className="form-check-label" htmlFor="inlineCheckboxMd5">MD5</label>
           </div>
           <div className="form-check form-check-inline">
-            <input className="form-check-input" type="checkbox" id="inlineCheckboxSha1" value="sha1" />
+            <input className="form-check-input" type="checkbox" id="inlineCheckboxSha1" checked={sha1Checkbox} value="sha1"
+                onChange={setSha1Checkbox(!sha1Checkbox)} />
             <label className="form-check-label" htmlFor="inlineCheckboxSha1">SHA-1</label>
           </div>
           <div className="form-check form-check-inline">
-            <input className="form-check-input" type="checkbox" id="inlineCheckboxSha256" value="sha256"/>
+            <input className="form-check-input" type="checkbox" id="inlineCheckboxSha256" checked={sha256Checkbox} value="sha256"
+                onChange={setSha256Checkbox(!sha256Checkbox)} />
             <label className="form-check-label" htmlFor="inlineCheckboxSha256">SHA-256</label>
           </div>
         </div>
-        <span><small>Current Algorithms: ???</small></span>
+        <span><small>Current Algorithms: {booleanDictToString(algorithmsServer)}</small></span>
       </div>
       <div className="col-sm-12 col-md-auto align-self-start">
         <button onClick={e => createBloomFilter(e)} className="btn btn-primary btn-sm">Create</button>
