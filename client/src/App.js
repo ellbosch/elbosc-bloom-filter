18e193985bed153ee8d4b38b87732b84fff61b64
Elliot Boschwitz <elliot.boschwitz@gmail.com>
2019-08-10 19:13:20 -0700

    UI updates with bootstrap alert on query

--- client/src/App.js
+++ client/src/App.js
@@ -2,7 +2,21 @@ import React, { useState, useEffect } from 'react';
 
 function App() {
   const [queryString, setQueryString] = useState("");
+
+  return (
+    <div className="container text-center">
+      <h1>Bloom Filter</h1>
+      <input type="text" className="form-control" placeholder="Look up word in bloom filter" aria-label="Search word" aria-describedby="button-search" value={queryString} onChange={e => setQueryString(e.target.value)} />
+      <br />
+      <QueryResult query={queryString} />
+    </div>
+  );
+}
+
+function QueryResult(props) {
+  const queryString = props.query;
   const [result, setResult] = useState(null);
+  const [alertClass, setAlertClass] = useState(null);   // determines color of alert
 
   useEffect(() => {
     fetch('/bloomfilter/' + queryString)
@@ -11,20 +25,18 @@ function App() {
 			(result) => {
         if (result.contains) {
           setResult("Bloom filter may contain \"" + queryString + "\"");
+          setAlertClass("alert alert-primary");
         } else {
           setResult("Bloom filter does not contain \"" + queryString + "\"");
+          setAlertClass("alert alert-warning");
         }
 			}
 		)
   });
 
   return (
-    <div className="container">
-      <h1>Bloom Filter</h1>
-      <input type="text" className="form-control" placeholder="Look up word in bloom filter" aria-label="Search word" aria-describedby="button-search" value={queryString} onChange={e => setQueryString(e.target.value)} />
-      <div>
-        {result}
-      </div>
+    <div className={alertClass} role="alert">
+      {result}
     </div>
   );
 }
