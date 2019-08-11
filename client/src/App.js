77a73b593999bda40ec7f5e791146c50b5846a60
Elliot Boschwitz <elliot.boschwitz@gmail.com>
2019-08-10 19:02:51 -0700

    UI updates in real time with query

--- client/src/App.js
+++ client/src/App.js
@@ -1,60 +1,32 @@
 import React, { useState, useEffect } from 'react';
-import { BrowserRouter as Router, Route, Link } from "react-router-dom";
 
 function App() {
-  return (
-    <Router>
-      <div className="container">
-        <h1>Hello!</h1>
-        <Route path="/" exact component={Home} />
-        <Route path="/bloomfilter" component={BloomFilter} />
-        <Route path="/bloomfilter/:word" component={SpellChecker} />
-      </div>
-    </Router>
-  );
-}
+  const [queryString, setQueryString] = useState("");
+  const [result, setResult] = useState(null);
 
-function Home() {
-  return (
-    <h2>Home</h2>
-  )
-}
-
-// create bloom filter
-function BloomFilter() {
-	// fetch response from server
-	useEffect(() => {
-		fetch('/bloomfilter')
-	});
-
-	return (
-		<h2>Bloom Filter</h2>
-	)
-}
-
-function SpellChecker({match}) {
-  const [result, setResult] = useState(null);  
-  const word = match.params.word;
-
-  // fetch response from server
-	useEffect(() => {
-    fetch('/bloomfilter/' + word)
+  useEffect(() => {
+    fetch('/bloomfilter/' + queryString)
     .then(res => res.json())
 		.then(
 			(result) => {
-        console.log(result.contains);
         if (result.contains) {
-          setResult("Bloom filter may contain \"" + word + "\"");
+          setResult("Bloom filter may contain \"" + queryString + "\"");
         } else {
-          setResult("Bloom filter does not contain \"" + word + "\"");
+          setResult("Bloom filter does not contain \"" + queryString + "\"");
         }
 			}
 		)
   });
-  
+
   return (
-    <h4>{result}</h4>
-  )
+    <div className="container">
+      <h1>Bloom Filter</h1>
+      <input type="text" className="form-control" placeholder="Look up word in bloom filter" aria-label="Search word" aria-describedby="button-search" value={queryString} onChange={e => setQueryString(e.target.value)} />
+      <div>
+        {result}
+      </div>
+    </div>
+  );
 }
 
 export default App;
